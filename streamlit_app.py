bbd38786cb721a6147cd9321ad9142c9200d167d
Konstantin Schulz <Konstantin.Schulz@dfki.de>
2021-12-23 15:52:51 +0100

    improved horizontal line

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,6 @@ from train import train_sklearn
 train_sklearn()
 
 st.title('Wie feministisch bist du?')
-st.markdown("---")
 st.subheader('Beispiele')
 sample_text: str = "Das Männerbild der meisten Feministinnen ist unverrückbar. Die wollen gar keinen einsichtigen, reflektierten Mann, die wollen einen Punch Bag zum nimmermüden Draufhauen."
 st.info(f'"{sample_text}" (Christian Ulmen)')
@@ -15,7 +14,7 @@ st.subheader(f"Femi-Score: {predict_score(sample_text)}% {Reaction.bad.value}")
 sample_text = "Spivak strebt mit ihren Überlegungen zur Finanzialisierung des Ländlichen an, die gegenwärtigen Veränderungen innerhalb kapitalistischer Verhältnisse aus einer feministischen sowie nichteurozentrischen Betrachtung zu begreifen."
 st.info(f'"{sample_text}"')
 st.subheader(f"Femi-Score: {predict_score(sample_text)}% {Reaction.good.value}")
-st.markdown("---")
+st.markdown("""<hr style="height:.3em;border:none;color:#333;background-color:#333;" /> """, unsafe_allow_html=True)
 st.subheader("Probier es selbst:")
 input_text: str = st.text_input('Eingabe', '', placeholder="Feminismus ist für alle da.")
 current_score: int = predict_score(input_text)
